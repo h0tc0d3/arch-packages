e21d10ab4129ed33c83ee3bf4f2d783fb496ffef
Grigory Vasilyev <h0tc0d3@gmail.com>
2021-06-18 23:30:08 +0300

    Add packages

--- build.sh
+++ build.sh
@@ -17,23 +17,25 @@ PACKAGES=(
   qt5-base icu freetype2 pango fontconfig harfbuzz cairo libepoxy
   wayland-protocols egl-wayland libx11 xtrans libice libsm libxt
   libxrender xorg-server xorgproto libxv libxpm libxfixes libxtst
-  xorg-xauth xorg-util-macros libxext libxres libxi libxmu libxkbfile 
+  xorg-xauth xorg-util-macros libxext libxres libxi libxmu libxkbfile
   libpciaccess xorg-font-util libxfont2 xorg-xkbcomp libxshmfence
-  xorg-setxkbmap kwin plasma-workspace plasma-framework glibc
-  xcb-proto libxcb xcb-util xcb-util-renderutil xcb-util-image
-  xcb-util-cursor xcb-util-keysyms xcb-util-wm libunwind
+  xorg-setxkbmap kwin plasma-workspace plasma-framework glibc libva
+  xcb-proto libxcb xcb-util xcb-util-renderutil xcb-util-image libxdamage
+  xcb-util-cursor xcb-util-keysyms xcb-util-wm libunwind libdrm libxxf86vm
+  libffi libvdpau expat wayland lm_sensors
 )
 
 IPACKAGES=(
   zlib-ng zstd libxml2 libjpeg-turbo libpng mesa qt5-base icu
   freetype2 fontconfig pango harfbuzz xorg-util-macros libxkbfile libxext
-  libxres libxi libpciaccess xorg-font-util libunwind
+  libxres libxi libpciaccess libdrm xorg-font-util libunwind
   libxfont2 xorg-xkbcomp xorg-setxkbmap xorg-xauth wayland-protocols
-  egl-wayland xorgproto libxfixes libxv pixman cairo libx11 xtrans libice libsm libxt
-  libxtst libxpm libxmu libxrender libepoxy libxshmfence xorg-server kwin
+  egl-wayland xorgproto libxxf86vm libxfixes libxdamage libxv pixman cairo
+  libx11 xtrans libice libsm libxt libxtst libxpm libxmu libxrender
+  libepoxy libxshmfence libva libvdpau xorg-server kwin libffi expat wayland
   plasma-workspace plasma-framework glib2 gtk2 gtk3 gtk4
   xcb-proto libxcb xcb-util xcb-util-renderutil xcb-util-image
-  xcb-util-cursor xcb-util-keysyms xcb-util-wm
+  xcb-util-cursor xcb-util-keysyms xcb-util-wm lm_sensors
 )
 
 install-keys() {
